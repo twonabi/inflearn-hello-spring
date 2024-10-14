0e163f7a8bbb99f7c3962b1f47c73f5ed9f469e4
jian <loveinansan1@gmail.com>
2024-10-14 14:22:51 +0900

    #20 inflearn-hello-spring
    class8_AOP_AOP가 필요한 상황
    
    - 모든 메소드의 호출 시간을 측정하고 싶다면?
    - 공통 관심 사항 vs 핵심 관심 사항
    - 회원 가입 시간, 회원 조회 시간을 측정하고 싶다면?

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -19,28 +19,45 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join (Member member) {
+    public Long join(Member member) {
 
-        validateDuplicationMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicationMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+
+
+        }
     }
 
     private void validateDuplicationMember(Member member) {
         memberRepository.findByName(member.getName())
-            .ifPresent(m -> {
-                throw new IllegalStateException("이미 존재하는 회원입니다.");
-            });
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
     /**
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
-    public Optional<Member> findOne (Long memderId) {
+    public Optional<Member> findOne(Long memderId) {
         return memberRepository.findById(memderId);
     }
 }
